2e3d61196acdb8cdb3d3b8f3592a80a07e0fd7f6
Paul Hong <paul@Pauls-MacBook-Pro.local>
2018-11-27 21:23:49 -0800

    react-beautiful-dnd

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import initialData from './initial-data';
+import Column from './column';
+import { DragDropContext } from 'react-beautiful-dnd';
 
+class App extends React.Component {
+    state = initialData;
+
+    onDragEnd = result => {
+        const { destination, source, draggableId } = result;
+
+        if(!destination) {
+            return;
+        }
+
+        if(
+            destination.droppableId === source.droppableId &&
+            destination.index === source.index
+        ) {
+            return;
+        }
+
+        const column = this.state.columns[source.droppableId];
+        const newTaskIds = Array.from(column.taskIds);
+        newTaskIds.splice(source.index, 1);
+        newTaskIds.splice(destination.index, 0, draggableId);
+    }
+
+    render() {
+        return (
+            <DragDropContext onDragEnd={this.onDragEnd}>
+            {this.state.columnOrder.map((columnId) => {
+                const column = this.state.columns[columnId];
+                const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
+
+                return <Column key={column.id} column={column} tasks={tasks} />;
+            })}
+            </DragDropContext>
+        )
+    }
+}
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
